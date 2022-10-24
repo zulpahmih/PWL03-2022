fd376c6786171f212c62ff00bbbad7fc10e55a0c
zulpahmih <zulmi473@gmail.com>
2022-10-23 19:34:04 -0700

    admin install

---
+++ resources/views/greeting.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Laravel</title>
+</head>
+
+<body>
+    <p>Hello there</p>
+    <p>Hello{{ $name }}</p>
+    <p>Hello{{ $kelas }}</p>
+</body>
+</html>
